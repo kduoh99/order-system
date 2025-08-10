d9716f9f8b914709f898c6cff4d3b69e9e75fe51
Kang Duoh <kduoh99@gmail.com>
2025-08-10 16:09:26 +0900

    remove: 불필요한 소스코드 제거

--- msa/member/src/main/java/com/study/ordersystem/common/config/SecurityConfig.java
+++ msa/member/src/main/java/com/study/ordersystem/common/config/SecurityConfig.java
@@ -1,58 +1,12 @@
 package com.study.ordersystem.common.config;
 
-import java.util.Arrays;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import com.study.ordersystem.common.auth.JwtAuthFilter;
-
-import lombok.RequiredArgsConstructor;
 
 @Configuration
-@RequiredArgsConstructor
-@EnableMethodSecurity
 public class SecurityConfig {
-
-	private final JwtAuthFilter authFilter;
-
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-		return httpSecurity
-			.cors(c -> c.configurationSource(corsConfiguration()))
-			.csrf(AbstractHttpConfigurer::disable)
-			.httpBasic(AbstractHttpConfigurer::disable)
-			.sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-			.addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
-			.authorizeHttpRequests(
-				a -> a.requestMatchers("/member/create", "/member/doLogin", "/member/refresh-token", "/product/list")
-					.permitAll()
-					.anyRequest()
-					.authenticated())
-			.build();
-	}
-
-	private CorsConfigurationSource corsConfiguration() {
-		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-		configuration.setAllowedMethods(Arrays.asList("*"));
-		configuration.setAllowedHeaders(Arrays.asList("*"));
-		configuration.setAllowCredentials(true);
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
-		return source;
-	}
 
 	@Bean
 	public PasswordEncoder makePassword() {
